24819ebd1384111e99d3914a2415bc65fec0c9fb
nayon117 <hasibul.nayon1@gmail.com>
2024-01-17 12:43:46 +0600

    create project structure

---
+++ InnavatED-client/src/components/Navber.jsx
@@ -0,0 +1,10 @@
+
+
+const Navber = () => {
+  return (
+    <div>
+      <h2>This is Navber</h2>
+    </div>
+  );
+};
+export default Navber;
---
+++ InnavatED-client/src/page/errorPage/ErrorPage.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="flex items-center justify-center py-12">
+      <div className="bg-white rounded-md flex items-center justify-center mx-4 md:w-2/3">
+        <div className="flex flex-col items-center py-16">
+          <img
+            className="px-4 hidden md:block"
+            src="https://i.ibb.co/9Vs73RF/undraw-page-not-found-su7k-1-3.png"
+          />
+          <img
+            className="md:hidden"
+            src="https://i.ibb.co/RgYQvV7/undraw-page-not-found-su7k-1.png"
+          />
+          <h1 className="px-4 pt-8 pb-4 text-center  text-5xl font-bold leading-10 text-gray-800">
+            OOPS!
+          </h1>
+          <p className="px-4 pb-10 text-base  text-center text-gray-600">
+            No signal here! we cannot find the page you are looking for
+          </p>
+          <Link to="/">
+            <button className="mx-4 h-10 w-44  rounded-md text-white text-base bg-indigo-700 hover:bg-indigo-800 ">
+              Go Back
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
