61f168771b74e0a1d95b63cdceb218e9afcf55d2
nayon117 <hasibul.nayon1@gmail.com>
2024-01-22 18:41:11 +0600

    add all courses page

--- InnavatED-client/vite.config.js
+++ InnavatED-client/vite.config.js
@@ -4,13 +4,8 @@ import tailwindcssForms from '@tailwindcss/forms';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-})
-=======
   plugins: [
     react(),
     tailwindcssForms(),
   ],
 });
->>>>>>> 5146f0dfd5c9f0705c25b85f6b7cf5e713d11d31
