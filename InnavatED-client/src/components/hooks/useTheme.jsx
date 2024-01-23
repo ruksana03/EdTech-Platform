df9356c9aa41b4a02fab47c16c8a6a250092914c
Sushil Hemrom <sushil500530@gmail.com>
2024-01-23 11:36:25 +0600

    add local store of mode

--- InnavatED-client/src/components/hooks/useTheme.jsx
+++ InnavatED-client/src/components/hooks/useTheme.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useTheme = () => {
     const [mode, setMode] = useState('light');
+
+    useEffect(() => {
+        const currentMode = localStorage.getItem("mode") || ("light");
+        const html = document.documentElement;
+        html.classList.add(currentMode);
+        setMode(currentMode)
+    }, [])
+
     const changeTheme = () => {
         const html = document.documentElement;
         if (mode === 'light') {
             html.classList.remove('light');
             html.classList.add('dark');
             setMode('dark');
-
+            localStorage.setItem("mode", "dark")
         }
         else {
             html.classList.remove('dark');
             html.classList.add('light');
             setMode('light');
+            localStorage.setItem("mode", "light")
         }
     }
     return { changeTheme, mode };
